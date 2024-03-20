27fcd0c2fb99ad34067c7a277d7e03d5540d820f
Michael Collins <mfcollins3@me.com>
2024-03-20 03:38:56 +0000

    Style homepage
    I deleted the dummy content from the normal theme. I added a placeholder
    for my first blog post. I created the homepage layout to show a
    responive navigation bar and a cover photo.

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Copyright 2024 Michael F. Collins, III
+#
+# Michael Collins's Website © 2024 by Michael F. Collins, III is licensed
+# under CC BY 4.0. To view a copy of this license, visit 
+# http://creativecommons.org/licenses/by/4.0/
+
+. ${NVM_DIR}/nvm.sh
+
+npm ci
+
+bundle install
+
+npm install -g @azure/static-web-apps-cli
+npm i -g azure-functions-core-tools@4 --unsafe-perm true
