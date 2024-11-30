015fd32d6354708a0b2465a92b8a50ac5ad0f4ef
Michael Collins <mfcollins3@me.com>
2024-11-30 22:29:10 +0000

    Stopping here

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -27,3 +27,44 @@
 # before the user is logged in. This program is run as the root user.
 
 npm ci
+
+bundle install
+
+pip install -r requirements.txt
+
+# Install diagram generators for asciidoctor-diagram
+go install github.com/asciitosvg/asciitosvg/cmd/a2s@latest
+curl -fsSL https://d2lang.com/install.sh | sh -s --
+
+mkdir -p /tmp/pikchr
+pushd /tmp/pikchr > /dev/null
+wget -O pikchr.c https://pikchr.org/home/raw/85e65b968651b342c46e6334f4772b45d6cbb4317c5cbaa95d207779a50c6709?at=pikchr.c
+gcc -DPIKCHR_SHELL -o pikchr pikchr.c -lm
+sudo mv pikchr /usr/local/bin
+popd > /dev/null
+rm -rf /tmp/pikchr
+
+ROOT_DIR=$(pwd)
+mkdir -p bin/structurizr-cli
+mkdir -p /tmp/structurizr-cli
+pushd /tmp/structurizr-cli > /dev/null
+wget https://github.com/structurizr/cli/releases/download/v2024.11.04/structurizr-cli.zip
+unzip -d $ROOT_DIR/bin/structurizr-cli -o -qq structurizr-cli.zip
+popd > /dev/null
+rm -rf /tmp/structurizr-cli
+
+pushd /tmp > /dev/null
+git clone --depth 1 --branch 7.1.1-41 https://github.com/ImageMagick/ImageMagick.git ImageMagick-7.1.1
+cd ImageMagick-7.1.1
+./configure
+make
+sudo make install
+sudo ldconfig /usr/local/lib
+popd > /dev/null
+rm -rf /tmp/ImageMagick-7.1.1
+
+pushd /tmp > /dev/null
+wget https://github.com/atp-mipt/jsyntrax/releases/download/1.38/jsyntrax-1.38-syntrax.zip
+unzip -d $ROOT_DIR/bin -o -qq jsyntrax-1.38-syntrax.zip
+popd > /dev/null
+rm -rf /tmp/jsyntrax-1.38-syntrax.zip
